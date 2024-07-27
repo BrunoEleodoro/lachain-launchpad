ab37831463b19d6d091a97247f1eb821b4e93247
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2024-07-27 19:12:55 -0300

    upload images to S3 aws

--- web/app/api/uploadImage/route.ts
+++ web/app/api/uploadImage/route.ts
@@ -1,27 +1,54 @@
-import { NextResponse } from "next/server";
-import { revalidatePath } from "next/cache";
-import fs from "node:fs/promises";
-import path from "path";
+import { NextResponse } from 'next/server';
+import { revalidatePath } from 'next/cache';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import path from 'path';
+
+const s3Client = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
+
+const MAX_FILE_SIZE = 10 * 1024 * 1024; // 10MB in bytes
 
 export async function POST(req: Request) {
   try {
     const formData = await req.formData();
 
-    const file = formData.get("file") as File;
+    const file = formData.get('file') as File;
+    
+    if (file.size > MAX_FILE_SIZE) {
+      return NextResponse.json({ status: 'fail', error: 'File size exceeds 10MB limit' }, { status: 400 });
+    }
+
     const arrayBuffer = await file.arrayBuffer();
-    const buffer = new Uint8Array(arrayBuffer);
+    const buffer = Buffer.from(arrayBuffer);
 
     const timestamp = Date.now();
     const fileExtension = path.extname(file.name);
     const newFileName = `${timestamp}${fileExtension}`;
 
-    await fs.writeFile(`./public/uploads/${newFileName}`, buffer);
+    const params = {
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: newFileName,
+      Body: buffer,
+      ContentType: file.type,
+    };
 
-    revalidatePath("/");
+    const command = new PutObjectCommand({
+      ...params,
+      ACL: 'public-read',
+    });
+    await s3Client.send(command);
 
-    return NextResponse.json({ status: "success", imageUrl: `/uploads/${newFileName}` });
+    revalidatePath('/');
+
+    const imageUrl = `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${newFileName}`;
+    return NextResponse.json({ status: 'success', imageUrl });
   } catch (e) {
     console.error(e);
-    return NextResponse.json({ status: "fail", error: e });
+    return NextResponse.json({ status: 'fail', error: e });
   }
 }
